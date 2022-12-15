cbdc6d9ca3b004b2d4a345573e911b07ed0f9fbb
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-15 00:37:44 -0300

    fix: Fixing modules stucture

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,10 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification-use-case';
 import { createNotificationDTO } from '../dtos/create-notification-dto';
 
 @Controller('notifications')
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @Post()
   async create(@Body() data: createNotificationDTO) {
     const { content, category, recipientId } = data;
+
+    const { notification } = await this.sendNotification.execute({
+      category,
+      content,
+      recipientId,
+    });
+
+    return { notification };
   }
 }
