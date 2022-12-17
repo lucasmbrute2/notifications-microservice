3725c544635784904f479fcf32f47ca7be5ac310
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-16 22:43:39 -0300

    feat: added notification repository

---
+++ src/application/repositories/notifcations-repository.ts
@@ -0,0 +1,8 @@
+import { Notification } from 'src/application/entities/notification';
+
+export abstract class NotificationRepository {
+  abstract create(notifcation: Notification): Promise<void>;
+  abstract findById(id: string): Promise<Notification | null>;
+  abstract save(notification: Notification): Promise<void>;
+  abstract countManyByRecipientId(id: string): Promise<number>;
+}
