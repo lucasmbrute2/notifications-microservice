c76d9633f0ff26fa93f5d69c21854bdf624b187a
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-16 22:43:56 -0300

    feat: implemented count recipient spec

---
+++ src/application/use-cases/count-recipient-notification.spec.ts
@@ -0,0 +1,44 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotifcationRepository } from '@test/repositories/in-memory-notifcations-repositories';
+import { CountRecipientsNotifications } from './count-recipient-notifications';
+
+describe('Count recipients notifications', () => {
+  it('should be able to count recipients', async () => {
+    const notificationsRepository = new InMemoryNotifcationRepository();
+    const countRecipientUseCase = new CountRecipientsNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('A beauty notification'),
+        recipientId: 'recipient-1',
+      }),
+    );
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('A beauty notification'),
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content('A beauty notification'),
+        recipientId: 'recipient-2',
+      }),
+    );
+
+    const { count } = await countRecipientUseCase.execute({
+      recipientId: 'recipient-1',
+    });
+
+    console.log(count);
+
+    expect(count).toEqual(2);
+  });
+});
