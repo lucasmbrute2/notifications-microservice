7abf90612d397c13b80736fec9490cd58cb24cd8
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-16 22:42:52 -0300

    feat: implemented findById, save and countManyByRecipient methods

--- test/repositories/in-memory-notifcations-repositories.ts
+++ test/repositories/in-memory-notifcations-repositories.ts
@@ -1,10 +1,34 @@
 import { Notification } from 'src/application/entities/notification';
-import { NotificationRepository } from 'src/repositories/notifcations-repository';
+import { NotificationRepository } from '@application/repositories/notifcations-repository';
 
 export class InMemoryNotifcationRepository implements NotificationRepository {
   public notifications: Notification[] = [];
 
+  async findById(id: string): Promise<Notification | null> {
+    const notifcation = this.notifications.find(
+      (notificationInMemo) => notificationInMemo.id === id,
+    );
+
+    if (!notifcation) return null;
+    return notifcation;
+  }
+
   async create(notifcation: Notification) {
     this.notifications.push(notifcation);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notifcationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notifcationIndex >= 0) {
+      this.notifications[notifcationIndex] = notification;
+    }
+  }
+
+  async countManyByRecipientId(id: string): Promise<number> {
+    return this.notifications.filter((notification) => notification.id === id)
+      .length;
   }
 }
