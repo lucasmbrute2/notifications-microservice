0d1e908ce580ac92ec9c984f661d3ebba76af0e9
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-16 23:07:24 -0300

    fix: setting as optional the first argument in Notification factory function

--- test/factories/notification-factory.ts
+++ test/factories/notification-factory.ts
@@ -6,7 +6,7 @@ import {
 
 type Override = Partial<NotificationProps>;
 
-export function makeNotification(override: Override): Notification {
+export function makeNotification(override?: Override): Notification {
   return new Notification({
     category: 'social',
     content: new Content('A beauty notification'),
