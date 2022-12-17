7ba741a282e15c8ec74744705381504ec4c98b97
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-17 15:28:07 -0300

    feat: Implemented toDomain method

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notifcation: Notification) {
@@ -10,5 +12,17 @@ export class PrismaNotificationMapper {
       readAt: notifcation.readAt,
       createdAt: notifcation.readAt,
     };
+  }
+
+  static toDomain(raw: RawNotification) {
+    const { content, id, ...rest } = raw;
+
+    return new Notification(
+      {
+        content: new Content(content),
+        ...rest,
+      },
+      id,
+    );
   }
 }
