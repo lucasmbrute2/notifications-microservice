a77382226f60b8e54be0c4e159f40eed0402f37b
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-18 16:34:01 -0300

    feat: configuring swagger

--- src/infra/http/dtos/create-notification-dto.ts
+++ src/infra/http/dtos/create-notification-dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class createNotificationDTO {
+  @ApiProperty()
   @IsNotEmpty()
   @Length(5, 240)
   content: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   category: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsUUID()
   recipientId: string;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.serv
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices/interfaces';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +15,15 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     strategy: kafkaConsumerService,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Notifications app')
+    .setDescription('Handle notifcations operations')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.startAllMicroservices();
   await app.listen(3000);
